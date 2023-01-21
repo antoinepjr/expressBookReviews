a353cf3e2be910504e9147e62b05a0f84b088b00
Antoine <antoinepjr@gmail.com>
2023-01-21 00:44:28 -0500

    Final modifications

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "isbn": "48785" , "reviews": {} },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "isbn": "12345", "reviews": {} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "isbn": "78945", "reviews": {} },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "isbn": "75315", "reviews": {} },
+      5: {"author": "Unknown","title": "The Book Of Job", "isbn": "95431", "reviews": {} },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "isbn": "12354", "reviews": {} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "isbn": "48567", "reviews": {} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "isbn": "46658", "reviews": {} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "isbn": "48784", "reviews": {} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "isbn": "48778", "reviews": {1: "Amazing Trilogy, book 2 is the best!"} }
 }
 
 module.exports=books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,102 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.query.username;
+    const password = req.query.password;
+
+    for(i = 0; i < users.length; i++){
+        if (users[i].username === username) {
+            res.send("Username already exists!");
+        }
+    }
+
+    if (!username || !password){
+        res.send("Username or Password does not exist.");
+    }
+
+    users.push({"username":req.query.username, "password":req.query.password});
+    res.send("The user" + (' ') + (req.query.username) + " has been added!")
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(res.send(JSON.stringify(books)));    
+        },2000)})
+    
+    myPromise.then((successMessage) => {
+        res.send(successMessage);    
+    })    
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    const isbn = req.params.isbn;    
+    
+    let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            for(i = 1; i < 11, i++;) {
+                if (books[i].isbn === isbn) {
+                    resolve(res.send(books[i]));
+                }
+            }  
+        }, 3000)})
+
+    myPromise.then((successMessage) => {
+        res.send(successMessage);
+    })
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            for(i = 1; i < 11, i++;) {
+                if (books[i].author === author) {
+                    resolve(res.send(books[i]));
+                }
+            }  
+        }, 3000)})
+
+    myPromise.then((successMessage) => {
+        res.send(successMessage);
+    })
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            for(i = 1; i < 11, i++;) {
+                if (books[i].title === title) {
+                    resolve(res.send(books[i]));
+                }
+            }  
+        }, 3000)})
+
+    myPromise.then((successMessage) => {
+        res.send(successMessage);
+    })
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            for(i = 1; i < 11, i++;) {
+                if (books[i].isbn === isbn) {
+                    resolve(res.send(books[i].reviews));
+                }
+            }  
+        }, 3000)})
+
+    myPromise.then((successMessage) => {
+        res.send(successMessage);
+    })
 });
 
 module.exports.general = public_users;
